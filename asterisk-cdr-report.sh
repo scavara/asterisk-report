e3e1bd5ae81b3ea1bb08d07ee9b83ce3a05d39bb
Sasa Cavara <scavara@gmail.com>
2012-06-12 10:41:22 +0200

    some instructions in README. some translation in the script.

--- asterisk-cdr-report.sh
+++ asterisk-cdr-report.sh
@@ -9,8 +9,10 @@ fi
 ASTLOGDIR=/var/log/asterisk/cdr-csv
 TMPDIR=/tmp
 TMPFILE=cdrfiltered
-grep -i "voopix\=odlazni" $ASTLOGDIR/Master-*-$MONTH-$YEAR_*.csv  | awk 'BEGIN{ FS=","; print "Pozivatelj\tBirani broj\tPocetak \t \tAnswer \t \t \tKraj\t \t \tTrajanje(s)"; } { if ($12 == "") print $2"\t"$3"\t"$11"\t""\t\"NO ANSWER\"\t"$13"\t"$15; if ($12 != "") print $2"\t"$3"\t"$11"\t"$12"\t"$13"\t"$15;}' > $TMPDIR/$TMPFILE
+USERFIELD=somethingyouwanttofilterby
+grep -i "$USERFIELD" $ASTLOGDIR/Master-*-$MONTH-$YEAR_*.csv  | awk 'BEGIN{ FS=","; print "Caller\tDailed number\tStart \t \tAnswer \t \t \tEnd\t \t \tDuration(s)"; } { if ($12 == "") print $2"\t"$3"\t"$11"\t""\t\"NO ANSWER\"\t"$13"\t"$15; if ($12 != "") print $2"\t"$3"\t"$11"\t"$12"\t"$13"\t"$15;}' > $TMPDIR/$TMPFILE
 
+#total number of calls
 BR_POZIVA_TOTAL=`cat /tmp/cdrfiltered | wc -l`
 totalsec=0
 for sec in `cat $TMPDIR/$TMPFILE | sed 1d | awk -F'\t' '{ if ($5 !~ "NO ANSWER") print $6}'`
@@ -18,17 +20,18 @@ do
 secinline=`echo ${sec} | cut -f 1`
 totalsec=`expr ${totalsec} + ${secinline}`  
 done
+#total duration in minutes
 totalmin=`expr $totalsec / 60`
 
-echo "Asterisk CDR izvjestaj za $(hostname) 
-Mjesec/godina: $MONTH/$YEAR
-Ukupan broj besplatnih poziva: $BR_POZIVA_TOTAL
-Ukupan broj besplatnih minuta: $totalmin
+echo "Asterisk CDR report for $(hostname) 
+Month/Year: $MONTH/$YEAR
+Total number of USERFIELD filtered calls: $BR_POZIVA_TOTAL
+Total duration of USERFIELD filtered calls in minutes: $totalmin
 --
-Vas voopIX tim
+YOUR SIGNATURE
 
-" > $TMPDIR/email2fkit-msg.txt 
-cat $TMPDIR/$TMPFILE >> $TMPDIR/email2fkit-att.txt 
+" > $TMPDIR/email2user-msg.txt 
+cat $TMPDIR/$TMPFILE >> $TMPDIR/email2user-att.txt 
 rm $TMPDIR/$TMPFILE
 exit 0
 
